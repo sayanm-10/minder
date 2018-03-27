4a046cb4b6d8f1146f8c0f36e862de197c2301df
Sayan Mukherjee <sayan.mukherjee108@gmail.com>
2018-03-27 17:28:43 -0400

    complete reducer with actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,8 @@
+// Function is an Action Creator
+// Returns an Action (an obj)
 export const selectBook = (book) => {
     return {
-
+        type: 'BOOK_SELECTED',
+        payload: book
     };
 }
