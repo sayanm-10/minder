701d60ddda25dde4ea03e127e6044fbdb4a6e5d5
Sayan Mukherjee <sayan.mukherjee108@gmail.com>
2018-03-27 13:55:38 -0400

    have fun with reducers

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        {title : "A Feast for Crows"},
+        {title : "The Hobbit"},
+        {title : "Empire of Moghul"},
+        {title : "Neuromancer"}        
+    ]
+}
